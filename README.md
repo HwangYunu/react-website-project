aa93f51953f1abc7ae496f1f216ba1605daba672
HwangYunu <lybear108@gmail.com>
2022-11-21 17:01:14 +0900

    firebase 연결

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # React와 Firebase를 이용한 프로젝트 입니다.
+
+### .env - FirebaseConfig
--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css'
+import { db } from './firebase.js'
+import { useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    db.collection('todo')
+      .get()
+      .then(result => {
+        result.forEach(doc => {
+          console.log(doc.data())
+        })
+      })
+  })
+
   return (
     <div className="App">
       <h1>Hello React!</h1>
+      <h1>zz</h1>
     </div>
   )
 }
